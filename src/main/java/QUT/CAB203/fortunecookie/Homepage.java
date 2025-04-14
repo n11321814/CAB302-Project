068e3aeda5cb1e800b4bfe21b87fcfc1a783299e
jay8999 <n12059099@qut.edu.au>
2025-04-14 15:20:26 +1000

    Homepage UI files
    Hey guys just made the homepage fxml file and controller for it, when the homepage is finished by AP ill start designing it, I see Nic made a start on the login and registration files so ill wait until Sylas is finished with them before i make any changes to the FXML file.

---
+++ src/main/java/QUT/CAB203/fortunecookie/Homepage.java
@@ -0,0 +1,13 @@
+package QUT.CAB203.fortunecookie;
+
+import javafx.fxml.FXML;
+
+public class Homepage {
+
+    @FXML
+    private void handleClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+
+// This is the controller for homepage.fxml, for when homepage is finished.
