3e24e5658d53821d165a605cbbc5d0a10452195e
SylasWoodall <woodall_sylas@hotmail.com>
2025-04-29 15:33:36 +1000

    Moved Old Files to LegacyFiles

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module address.book.addressbook {
 
     opens QUT.CAB203.fortunecookie to javafx.fxml;
     exports QUT.CAB203.fortunecookie;
+    exports QUT.CAB203.fortunecookie.LegacyFiles;
+    opens QUT.CAB203.fortunecookie.LegacyFiles to javafx.fxml;
 }
